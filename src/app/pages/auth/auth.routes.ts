92ce276a8419bf6ceeb842896a4497fe4b38f23e
Steven Razafimahery <stevenmahery@gmail.com>
2025-03-31 21:44:14 +0300

    Update routes gmail-reset

--- src/app/pages/auth/auth.routes.ts
+++ src/app/pages/auth/auth.routes.ts
@@ -3,9 +3,13 @@ import { Access } from './access';
 import { Login } from './login';
 import { Error } from './error';
 import { SignUp } from './signUp';
+import { ForgotPassword } from './forgot-password';
+import { ResetPassword } from './reset-password';
 export default [
     { path: 'access', component: Access },
     { path: 'error', component: Error },
     { path: 'login', component: Login },
-    { path: 'signUp', component: SignUp }
+    { path: 'signUp', component: SignUp },
+    { path: 'forgot-password', component: ForgotPassword },
+    { path: 'reset-password', component: ResetPassword }
 ] as Routes;
