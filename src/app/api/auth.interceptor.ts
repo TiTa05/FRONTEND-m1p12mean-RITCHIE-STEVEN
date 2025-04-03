1a923d181a9b96fd1aed4933f506d1adcdc1d64f
TiTa05 <stevensmex05@gmail.com>
2025-04-03 13:28:18 +0300

    Update auth.interceptor.ts
    handling error 401

--- src/app/api/auth.interceptor.ts
+++ src/app/api/auth.interceptor.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@angular/core';
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HttpResponse } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HttpResponse, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { tap, catchError } from 'rxjs/operators';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
+    constructor(private router: Router) {}
+
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         return next.handle(req).pipe(
             tap({
@@ -13,12 +16,9 @@ export class AuthInterceptor implements HttpInterceptor {
                         const body = event.body;
                         if (body && body.extractToken) {
                             const authorizationHeader = event.headers.get('Authorization');
-                            if (authorizationHeader) {
-                                if (authorizationHeader.startsWith('Bearer ')) {
-                                    const token = authorizationHeader.replace('Bearer ', '');
-
-                                    localStorage.setItem('token', token);
-                                }
+                            if (authorizationHeader && authorizationHeader.startsWith('Bearer ')) {
+                                const token = authorizationHeader.replace('Bearer ', '');
+                                localStorage.setItem('token', token);
                             }
                         }
                     }
@@ -26,6 +26,14 @@ export class AuthInterceptor implements HttpInterceptor {
                 error: (error) => {
                     console.error('Erreur interceptée :', error);
                 }
+            }),
+            catchError((error: HttpErrorResponse) => {
+                if (error.status === 401) {
+                    console.warn('Erreur 401 : Redirection vers login');
+                    localStorage.removeItem('token');
+                    this.router.navigate(['/login']);
+                }
+                return throwError(() => error);
             })
         );
     }
